85bc8bd627f9bc9d360bec006c9c03cc54443043
SarraBoujmil <sarra.boujmil@gmail.com>
2023-05-11 19:12:01 +0100

    react app adding

---
+++ myproject1/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Composant1 from './composants/Composant1';
+import Formulaire from './composants/Formulaire';
+
+function App() {
+  return (
+    <div className="App">
+      <Composant1></Composant1> 
+      <Formulaire></Formulaire>
+      That's it for today!!
+    </div>
+  );
+}
+
+export default App;
